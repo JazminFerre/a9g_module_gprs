092fadf607239f0faf5c6b3ef8a197ece034f008
Jazchu <jantonini@fi.uba.ar>
2021-08-01 13:34:50 -0300

    agregue comentario

--- demo/gps/src/demo_gps.c
+++ demo/gps/src/demo_gps.c
@@ -18,7 +18,7 @@
 static HANDLE gpsTaskHandle = NULL;
 bool flag = false;
 bool isGpsOn = true;
-
+// tarda mucho en conectar
 
 // const uint8_t nmea[]="$GNGGA,000021.263,2228.7216,N,11345.5625,E,0,0,,153.3,M,-3.3,M,,*4E\r\n$GPGSA,A,1,,,,,,,,,,,,,,,*1E\r\n$BDGSA,A,1,,,,,,,,,,,,,,,*0F\r\n$GPGSV,1,1,00*79\r\n$BDGSV,1,1,00*68\r\n$GNRMC,000021.263,V,2228.7216,N,11345.5625,E,0.000,0.00,060180,,,N*5D\r\n$GNVTG,0.00,T,,M,0.000,N,0.000,K,N*2C\r\n";
 
